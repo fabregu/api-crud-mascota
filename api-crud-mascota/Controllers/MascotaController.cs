59b0ec7bd03c6fc9c8e8c8bb0911b82c5a501371
Wilfredo Abregu <fabregu01@gmail.com>
2025-11-02 10:17:59 -0500

    feat: AgregarEditarMascota

--- api-crud-mascota/Controllers/MascotaController.cs
+++ api-crud-mascota/Controllers/MascotaController.cs
@@ -68,5 +68,53 @@ namespace api_crud_mascota.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Mascota mascota)
+        {
+            try
+            {
+                mascota.FechaCreacion = DateTime.Now;
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("Get", new {id=mascota.Id}, mascota);
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Mascota mascota)
+        {
+            try
+            {
+                if (id != mascota.Id)
+                {
+                    return BadRequest();
+                }
+                var mascotaItem = await _context.Mascotas.FindAsync(id);
+
+                if (mascotaItem == null)
+                {
+                    return NotFound();
+                }
+
+                mascotaItem.Nombre = mascota.Nombre;
+                mascotaItem.Raza = mascota.Raza;
+                mascotaItem.Edad = mascota.Edad;
+                mascotaItem.Color = mascota.Color;
+                mascotaItem.Peso = mascota.Peso;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
